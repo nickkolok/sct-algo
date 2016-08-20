9a8768d05e838e7fc4728ea1c22779f9f0784c38
nickkolok <nickkolok@mail.ru>
2016-08-20 22:26:28 +0300

    Менеждер понимает аргументы командной строки

--- sct-manager.js
+++ sct-manager.js
@@ -1,31 +1,14 @@
 var solver = require('./sct-2.js');
 
-
-/*
-3 1
-4 4
-5 7
-6 8
-7 17
-8 24
-9 26..35
-
-34 553
-*/
-/*
-findSCTs(4,5);
-*/
-
 var found=0;
 function onfound(){
 	found = 1;
 }
 
-//var p=44;
-//var d=964;
-var p=14;
-var d=90;
-while(d<95){
+var p = process.argv[2] || 3;
+var d = process.argv[3] || 1;
+var l = process.argv[4] || 1000;
+while(d<=l){
 	found=0;
 	solver.startSearch(p,d,{onfound:onfound});
 	if(found){
