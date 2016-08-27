4ba91980d19ec045d2b2f7b9a027ebf2f4d189b7
nickkolok <nickkolok@mail.ru>
2016-08-27 10:05:54 +0300

    Универсальный раннер

--- sct-runner.js
+++ sct-runner.js
@@ -1,4 +1,6 @@
-const solver = require(process.argv[4] || './sct-2.js');
+const defaultSolver = './sct-2-2.js'/*'./sct-2-2-temp-3.js'*/;
+const realSolver = process.argv[4] || defaultSolver;
+const solver = require(realSolver);
 
 const targetPow = process.argv[2];
 const maxD = process.argv[3];
@@ -16,9 +18,23 @@ function retranslator(type){
 	}
 }
 
+var options = {
+	solver: realSolver,
+};
+try{
+	if(process.argv[5]){
+		var parsedOptions = JSON.parse(process.argv[5]);
+		for(var opt in parsedOptions){
+			options[opt] = parsedOptions[opt];
+		}
+	}
+}catch(e){
+	console.log('Не удалось распознать список опций');
+}
+
 solver.startSearch(targetPow,maxD,{
 	onfound: retranslator('found'),
 	onnotfound: retranslator('notfound'),
 	onfinished: retranslator('finished'),
 	ondumpsaved: retranslator('step'),
-});
+},options);
